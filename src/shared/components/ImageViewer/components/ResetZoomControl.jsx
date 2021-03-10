b0c548205e6a4e7007f0b178c1fb2e73b8f18819
Artem Zubkov <zubkov.artem@gene.com>
2021-03-10 20:21:17 +0300

    #2: add imageView fullscreen and reset zoom buttons

---
+++ src/shared/components/ImageViewer/components/ResetZoomControl.jsx
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+import L from 'leaflet';
+import { useMap } from 'react-leaflet';
+
+L.Control.ResetZoom = L.Control.extend({
+  options: {
+    position: 'topleft',
+  },
+
+  onAdd(map) {
+    this._map = map;
+
+    const container = L.DomUtil.create('div', 'leaflet-control-resetzoom leaflet-bar leaflet-control');
+
+    const link = L.DomUtil.create('a', 'leaflet-control-resetzoom-button leaflet-bar-part', container);
+    link.title = 'Reset Zoom';
+    link.href = '#';
+
+    L.DomEvent.on(link, 'click', this._click, this);
+    L.DomEvent.on(link, 'dblclick', this._click, this);
+
+    return container;
+  },
+
+  _click: function (e) {
+    L.DomEvent.stopPropagation(e);
+    L.DomEvent.preventDefault(e);
+    this._map.setView(this._map.getCenter(), this._map.options.zoom);
+  },
+});
+
+const FullscreenControl = (props) => {
+  const { position } = props;
+  const map = useMap();
+
+  useEffect(
+    () => {
+      const options = { position };
+      const resetZoomControl = new L.Control.ResetZoom(options);
+      map.addControl(resetZoomControl);
+    },
+    [map, position],
+  );
+
+  return null;
+};
+
+export default FullscreenControl;
--- src/shared/components/ImageViewer/index.jsx
+++ src/shared/components/ImageViewer/index.jsx
@@ -4,12 +4,20 @@ import L from 'leaflet';
 import { MapContainer, ZoomControl } from 'react-leaflet';
 import styled from 'styled-components';
 
+import FullscreenImg from '@/assets/leaflet/fullscreen.png';
+import FullscreenImg2x from '@/assets/leaflet/fullscreen@2x.png';
+import ResetZoom from '@/assets/leaflet/resetzoom.png';
+
+import FullscreenControl from './components/FullscreenControl';
 import OmeroLayer from './components/OmeroLayer';
+import ResetZoomControl from './components/ResetZoomControl';
+
 
 const baseUrl = 'http://idr.openmicroscopy.org/webgateway/render_image_region/';
 
 const ImageViewer = styled((props) => {
   const { className, data } = props;
+
   return (
     <MapContainer
       className={className}
@@ -17,7 +25,7 @@ const ImageViewer = styled((props) => {
       center={[0, 0]}
       minZoom={1}
       maxZoom={10}
-      zoom={2}
+      zoom={1}
       attributionControl={false}
       zoomControl={false}
     >
@@ -29,11 +37,59 @@ const ImageViewer = styled((props) => {
         }}
       />
       <ZoomControl position="bottomright" />
+      <ResetZoomControl position="bottomright" />
+      <FullscreenControl position="topright" />
     </MapContainer>
   );
 })`
   height: 100%;
   width: 100%;
+
+  // Fullscreen
+  .leaflet-control-fullscreen a {
+    background: #fff url(${FullscreenImg}) no-repeat 0 0;
+    background-size: 26px 52px;
+  }
+  &.leaflet-touch .leaflet-control-fullscreen a {
+    background-position: 2px 2px;
+  }
+  &.leaflet-fullscreen-on .leaflet-control-fullscreen a {
+    background-position: 0 -26px;
+  }
+  &.leaflet-touch.leaflet-fullscreen-on .leaflet-control-fullscreen a {
+    background-position: 2px -24px;
+  }
+
+  /* Do not combine these two rules; IE will break. */
+  &.leaflet-container:-webkit-full-screen {
+    width: 100% !important;
+    height: 100% !important;
+  }
+  &.leaflet-container.leaflet-fullscreen-on {
+    width: 100% !important;
+    height: 100% !important;
+  }
+
+  &.leaflet-pseudo-fullscreen {
+    position: fixed !important;
+    width: 100% !important;
+    height: 100% !important;
+    top: 0 !important;
+    left: 0 !important;
+    z-index: 99999;
+  }
+
+  @media (-webkit-min-device-pixel-ratio:2), (min-resolution:192dpi) {
+    .leaflet-control-fullscreen a {
+      background-image: url(${FullscreenImg2x});
+    }
+  }
+  
+  // Reset Zoom
+  .leaflet-control-resetzoom a {
+    background: #fff url(${ResetZoom}) no-repeat 6px 6px;
+    background-size: 18px 18px;
+  }
 `;
 
 export default ImageViewer;
