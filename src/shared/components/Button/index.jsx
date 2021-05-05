f7694ca9392080730e6c3bcf186b228012403dde
Artem Zubkov <zubkov.artem@gene.com>
2021-05-05 08:48:46 +0300

    #7: update button component

--- src/shared/components/Button/index.jsx
+++ src/shared/components/Button/index.jsx
@@ -1,12 +1,20 @@
-import Button from '@material-ui/core/Button';
+import ButtonOrigin from '@material-ui/core/Button';
+import styled from 'styled-components';
 
 const ButtonColors = {
   primary: 'primary',
-  secondary: 'secondary',
-  danger: 'danger',
+  secondary: 'default',
+  danger: 'secondary',
   tertiary: 'tertiary',
   link: 'link',
 };
+
+const Button = styled(ButtonOrigin).attrs((props) => ({
+  variant: props.variant || 'contained',
+  color: props.color || ButtonColors.primary,
+}))`
+
+`;
 
 export {
   Button as default,
