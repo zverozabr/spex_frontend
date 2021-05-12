dffb2ffccbe8bc3d7621bf9997e64851c1be1c73
Artem Zubkov <zubkov.artem@gene.com>
2021-05-12 08:33:09 +0300

    #7: add transferList component (alpha version)

---
+++ src/shared/components/TransferList/index.jsx
@@ -0,0 +1,225 @@
+/* eslint-disable react/jsx-sort-default-props */
+import React, { useState, useCallback } from 'react';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Button from '+components/Button';
+import Checkbox from '+components/Checkbox';
+
+const not = (a, b) => (a.filter((value) => b.indexOf(value) === -1));
+const intersection = (a, b) => (a.filter((value) => b.indexOf(value) !== -1));
+const union = (a, b) => ([...a, ...not(b, a)]);
+const onNoop = () => {};
+
+/**
+ * A transfer list (or "shuttle") enables the user to move one or more list items between lists.
+ */
+const TransferList = styled((props) => {
+  const {
+    className,
+    left,
+    right,
+    leftTitle,
+    rightTitle,
+    onChangeLeft,
+    onChangeRight,
+  } = props;
+
+  const [checked, setChecked] = useState([]);
+
+  const leftChecked = intersection(checked, left);
+  const rightChecked = intersection(checked, right);
+
+  const onCheckedLeft = useCallback(
+    () => {
+      onChangeLeft(left.concat(rightChecked));
+      onChangeRight(not(right, rightChecked));
+      setChecked(not(checked, rightChecked));
+    },
+    [checked, left, onChangeLeft, onChangeRight, right, rightChecked],
+  );
+
+  const onCheckedRight = useCallback(
+    () => {
+      onChangeRight(right.concat(leftChecked));
+      onChangeLeft(not(left, leftChecked));
+      setChecked(not(checked, leftChecked));
+    },
+    [checked, left, leftChecked, onChangeLeft, onChangeRight, right],
+  );
+
+  const numberOfChecked = useCallback(
+    (items) => intersection(checked, items).length,
+    [checked],
+  );
+
+  const onToggle = useCallback(
+    (value) => () => {
+      const currentIndex = checked.indexOf(value);
+      const newChecked = [...checked];
+
+      if (currentIndex === -1) {
+        newChecked.push(value);
+      } else {
+        newChecked.splice(currentIndex, 1);
+      }
+
+      setChecked(newChecked);
+    },
+  [checked],
+  );
+
+  const onToggleAll = useCallback(
+    (items) => () => {
+      if (numberOfChecked(items) === items.length) {
+        setChecked(not(checked, items));
+      } else {
+        setChecked(union(checked, items));
+      }
+    },
+  [checked, numberOfChecked],
+  );
+
+  const customList = useCallback(
+    (title, items) => (
+      <Card>
+        <CardHeader
+          avatar={
+            <Checkbox
+              onClick={onToggleAll(items)}
+              checked={numberOfChecked(items) === items.length && items.length !== 0}
+              indeterminate={numberOfChecked(items) !== items.length && numberOfChecked(items) !== 0}
+              disabled={items.length === 0}
+              inputProps={{ 'aria-label': 'all items selected' }}
+            />
+          }
+          title={title}
+          subheader={`${numberOfChecked(items)}/${items.length} selected`}
+        />
+        <Divider />
+        <List dense component="div" role="list">
+          {items.map((el) => {
+            const id = `transfer-list-all-item-${el.id}-label`;
+
+            return (
+              <ListItem key={el.id} role="listitem" button onClick={onToggle(el)}>
+                <ListItemIcon>
+                  <Checkbox
+                    checked={checked.indexOf(el) !== -1}
+                    tabIndex={-1}
+                    inputProps={{ 'aria-labelledby': id }}
+                    disableRipple
+                  />
+                </ListItemIcon>
+                <ListItemText id={id} primary={el.title} />
+              </ListItem>
+            );
+          })}
+          <ListItem />
+        </List>
+      </Card>
+    ),
+  [checked, numberOfChecked, onToggle, onToggleAll],
+  );
+
+  return (
+    <Grid
+      className={classNames('transfer-list', className || '')}
+      spacing={2}
+      justify="center"
+      alignItems="center"
+      wrap="nowrap"
+      container
+    >
+      <Grid
+        className={classNames('list', 'list-left')}
+        item
+      >
+        {customList(leftTitle, left)}
+      </Grid>
+
+      <Grid item>
+        <Grid
+          className="buttons-container"
+          direction="column"
+          alignItems="center"
+          container
+        >
+          <Button
+            variant="outlined"
+            size="small"
+            onClick={onCheckedRight}
+            disabled={leftChecked.length === 0}
+            aria-label="move selected right"
+          >
+            &gt;
+          </Button>
+          <Button
+            variant="outlined"
+            size="small"
+            onClick={onCheckedLeft}
+            disabled={rightChecked.length === 0}
+            aria-label="move selected left"
+          >
+            &lt;
+          </Button>
+        </Grid>
+      </Grid>
+
+      <Grid
+        className={classNames('list', 'list-right')}
+        item
+      >
+        {customList(rightTitle, right)}
+      </Grid>
+    </Grid>
+  );
+})`
+  width: 100%;
+  height: 100%;
+
+  .list {
+    height: 100%;
+    .MuiCard-root {
+      height: 100%;
+    }
+  }
+
+  .buttons-container {
+    ${Button} + ${Button} {
+      margin-top: 8px;
+    }
+  }
+
+`;
+
+TransferList.propTypes = {
+  className: PropTypes.string,
+  left: PropTypes.arrayOf(PropTypes.string),
+  right: PropTypes.arrayOf(PropTypes.string),
+  leftTitle: PropTypes.string,
+  rightTitle: PropTypes.string,
+  onChangeLeft: PropTypes.func,
+  onChangeRight: PropTypes.func,
+};
+
+TransferList.defaultProps = {
+  className: '',
+  left: [],
+  right: [],
+  leftTitle: 'Choices',
+  rightTitle: 'Chosen',
+  onChangeLeft: onNoop,
+  onChangeRight: onNoop,
+};
+
+export default TransferList;
