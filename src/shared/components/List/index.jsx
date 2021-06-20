48687517d035a04a0a05c7bce684b659b11efd68
Artem Zubkov <zubkov.artem@gene.com>
2021-06-20 13:41:42 +0300

    #8: add list component

---
+++ src/shared/components/List/index.jsx
@@ -0,0 +1,11 @@
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+export {
+  List as default,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+};
