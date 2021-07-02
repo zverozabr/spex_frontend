722bba547ebddfee82262d558226f0522cbf76d3
Artem Zubkov <zubkov.artem@gene.com>
2021-07-02 14:09:17 +0700

    added resource tasks tabs

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -5,6 +5,8 @@ import { matchPath, useLocation } from 'react-router-dom';
 import PathNames from '@/models/PathNames';
 import { actions as omeroActions, selectors as omeroSelectors } from '@/redux/modules/omero';
 import { actions as projectsActions, selectors as projectsSelectors } from '@/redux/modules/projects';
+import { actions as resourcesActions, selectors as resourcesSelectors } from '@/redux/modules/resources';
+import { actions as tasksActions, selectors as tasksSelectors } from '@/redux/modules/tasks';
 
 import Button, { ButtonColors } from '+components/Button';
 import ClickAwayListener from '+components/ClickAwayListener';
@@ -13,6 +15,8 @@ import MenuList, { MenuItem } from '+components/MenuList';
 import NoData from '+components/NoData';
 import Paper from '+components/Paper';
 import Popper from '+components/Popper';
+import Table from '+components/Table';
+import Tabs, { Tab, TabPanel } from '+components/Tabs';
 import ThumbnailsViewer from '+components/ThumbnailsViewer';
 
 import ButtonsContainer from './components/ButtonsContainer';
@@ -24,12 +28,13 @@ import PipelineContainer from './components/PipelineContainer';
 import Row from './components/Row';
 import ThumbnailsContainer from './components/ThumbnailsContainer';
 
+
 const not = (a, b) => (a.filter((value) => b.indexOf(value) === -1));
 
 const Project = () => {
   const dispatch = useDispatch();
   const { pathname } = useLocation();
-
+  const resources = useSelector(resourcesSelectors.getResources);
   const projectId = useMemo(
     () => {
       const match = matchPath(pathname, { path: `/${PathNames.projects}/:id` });
@@ -40,6 +45,7 @@ const Project = () => {
 
   const project = useSelector(projectsSelectors.getProject(projectId));
   const thumbnails = useSelector(omeroSelectors.getThumbnails(projectId));
+  const tasks = useSelector(tasksSelectors.getTasks);
 
   const anchorRef = useRef(null);
   const [open, setOpen] = useState(false);
@@ -48,10 +54,79 @@ const Project = () => {
   const [manageImagesModalOpen, setManageImagesModalOpen] = useState(false);
   const [manageJobsModalOpen, setManageJobsModalOpen] = useState(false);
   const [manageResourcesModalOpen, setManageResourcesModalOpen] = useState(false);
+  const [activeDataTab, setActiveDataTab] = useState(0);
 
   const omeroIds = useMemo(
     () => (project?.omeroIds || []),
   [project],
+  );
+  const resource_ids = useMemo(
+    () => (project?.resource_ids || []),
+  [project],
+  );
+  const taskIds = useMemo(
+    () => (project?.taskIds || []),
+  [project],
+  );
+
+  const r_actions = [{ name: 'Remove selected', fn: (rows) => onToggleRemoveRid(rows), color: ButtonColors.danger }];
+  const t_actions = [{ name: 'Remove selected', fn: (rows) => onToggleRemoveTid(rows), color: ButtonColors.danger }];
+
+  const r_columns = useMemo(
+    () => ([
+      {
+        id: 'name',
+        accessor: 'name',
+        Header: 'Name',
+        Cell: ({ row: { original: { id, name } } }) => useMemo(
+          () => (
+            // <Link to={`/${PathNames.resources}/${id}`}>
+            <div> {id} </div>
+            // </Link>
+          ),
+          [id],
+        ),
+      }, {
+        id: 'omeroIds',
+        accessor: 'omeroIds',
+        Header: 'Omero Image IDs',
+      },
+    ]),
+    [],
+  );
+  const t_columns = useMemo(
+    () => ([
+      {
+        id: 'id',
+        accessor: 'id',
+        Header: 'id',
+      },
+      {
+        id: 'name',
+        accessor: 'name',
+        Header: 'name',
+      },
+      ]),
+      [],
+    );
+
+  const res_data = useMemo(
+    () => {
+      if (resource_ids.length === 0 || resources.length === 0) {
+        return [];
+      };
+      return Object.values(resources).filter((res) => resource_ids.indexOf(res.id) > -1);
+    },
+    [resources, resource_ids],
+  );
+  const t_data = useMemo(
+    () => {
+      if (taskIds.length === 0 || tasks.length === 0) {
+        return [];
+      };
+      return Object.values(tasks).filter((task) => taskIds.indexOf(task.id) > -1);
+    },
+    [tasks, taskIds],
   );
 
   const normalizedThumbnails = useMemo(
@@ -177,6 +252,33 @@ const Project = () => {
     [dispatch],
   );
 
+  const onToggleRemoveRid = useCallback(
+    (rows) => {
+      const to_delete = rows.map((el) => el.id || el);
+      const new_data = resource_ids.filter((id) => to_delete.indexOf(id) === -1);
+      const updateData = { ...project, resource_ids: new_data };
+      dispatch(projectsActions.updateProject(updateData));
+    },
+    [dispatch, resource_ids, project],
+  );
+
+  const onToggleRemoveTid = useCallback(
+    (rows) => {
+      const to_delete = rows.map((el) => el.id || el);
+      const new_data = taskIds.filter((id) => to_delete.indexOf(id) === -1);
+      const updateData = { ...project, taskIds: new_data };
+      dispatch(projectsActions.updateProject(updateData));
+    },
+    [dispatch, taskIds, project],
+  );
+
+  const onDataTabChange = useCallback(
+    (_, id) => {
+      setActiveDataTab(id);
+    },
+    [],
+  );
+
   const prevOpen = React.useRef(open);
   useEffect(() => {
     if (prevOpen.current === true && open === false) {
@@ -185,6 +287,25 @@ const Project = () => {
 
     prevOpen.current = open;
   }, [open]);
+
+  useEffect(
+    () => {
+      if (resource_ids.length === 0) {
+        return;
+      }
+      dispatch(resourcesActions.fetchResources({}));
+    },
+    [dispatch, resource_ids],
+  );
+  useEffect(
+    () => {
+      if (taskIds.length === 0) {
+        return;
+      }
+      dispatch(tasksActions.fetchTasks({}));
+    },
+    [dispatch, taskIds],
+  );
 
   useEffect(
     () => {
@@ -207,13 +328,6 @@ const Project = () => {
     <Container>
       <Row>
         <ButtonsContainer>
-          <Button
-            color={ButtonColors.danger}
-            onClick={onRemoveImages}
-            disabled={selectedThumbnails.length === 0}
-          >
-            Remove Selected
-          </Button>
           <Button
             ref={anchorRef}
             aria-controls={open ? 'menu-list-grow' : undefined}
@@ -241,20 +355,55 @@ const Project = () => {
             )}
           </Popper>
         </ButtonsContainer>
-
-        <ThumbnailsContainer>
-          {normalizedThumbnails.length === 0 && <NoData>No Images To Display</NoData>}
-          {normalizedThumbnails.length > 0 && (
-            <ThumbnailsViewer
-              thumbnails={normalizedThumbnails}
-              active={selectedThumbnails}
-              onClick={onThumbnailClick}
-              allowMultiSelect
-              $size={1.5}
-              $center
-            />
-          )}
-        </ThumbnailsContainer>
+        <Tabs value={activeDataTab} onChange={onDataTabChange}>
+          <Tab label="Images" />
+          <Tab label="Resources" />
+          <Tab label="Tasks" />
+        </Tabs>
+        <TabPanel value={activeDataTab} index={0}>
+          <ButtonsContainer style={{ 'margin-bottom': '5px' }}>
+            <Button
+              color={ButtonColors.danger}
+              onClick={onRemoveImages}
+              disabled={selectedThumbnails.length === 0}
+            >
+              Remove Selected
+            </Button>
+          </ButtonsContainer>
+          <ThumbnailsContainer>
+            {normalizedThumbnails.length === 0 && <NoData>No Images To Display</NoData>}
+            {normalizedThumbnails.length > 0 && (
+              <ThumbnailsViewer
+                thumbnails={normalizedThumbnails}
+                active={selectedThumbnails}
+                onClick={onThumbnailClick}
+                allowMultiSelect
+                $size={1.5}
+                $center
+              />
+            )}
+          </ThumbnailsContainer>
+        </TabPanel>
+        <TabPanel value={activeDataTab} index={1}>
+          <Table
+            columns={r_columns}
+            data={res_data}
+            actions={r_actions}
+            allowRowSelection
+            pageSizeOptions={[10]}
+            minRows={10}
+          />
+        </TabPanel>
+        <TabPanel value={activeDataTab} index={2}>
+          <Table
+            columns={t_columns}
+            data={t_data}
+            actions={t_actions}
+            allowRowSelection
+            pageSizeOptions={[10]}
+            minRows={10}
+          />
+        </TabPanel>
       </Row>
 
       <Row>
