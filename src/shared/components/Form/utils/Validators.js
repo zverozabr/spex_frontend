404f7671993760809c17b2c40018be1646bcebe3
Artem Zubkov <zubkov.artem@gene.com>
2021-07-06 17:04:09 +0300

    #8: fix required and requiredConditional bug

--- src/shared/components/Form/utils/Validators.js
+++ src/shared/components/Form/utils/Validators.js
@@ -4,11 +4,17 @@ const required = (value) => {
   if (Array.isArray(value)) {
     return value.length ? undefined : 'Required';
   }
-  return value || value === 0 ? undefined : 'Required';
+  return value != null ? undefined : 'Required';
 };
 
 // @see: https://github.com/final-form/react-final-form/issues/372
-const requiredConditional = (fieldName) => (value, allValues) => get(allValues, fieldName) ? required(value) : undefined;
+const requiredConditional = (fieldName) => (value, allValues) => {
+  const conditionalFieldValue = get(allValues, fieldName);
+  if (Array.isArray(conditionalFieldValue)) {
+    return conditionalFieldValue.length ? required(value) : undefined;
+  }
+  return conditionalFieldValue ? required(value) : undefined;
+};
 
 const Validators = {
   required,
