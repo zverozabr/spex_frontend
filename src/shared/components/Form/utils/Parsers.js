1a0d1d97a76fea6c05685639212c3912003e7834
Artem Zubkov <zubkov.artem@gene.com>
2021-07-11 13:59:26 +0300

    #8: add numberBetween field parser

--- src/shared/components/Form/utils/Parsers.js
+++ src/shared/components/Form/utils/Parsers.js
@@ -1,7 +1,10 @@
 const number = (value) => (isNaN(parseFloat(value)) ? null : parseFloat(value));
 
+const numberBetween = (value, min, max) => Math.min(max, Math.max(min, number(value)));
+
 const Parsers = {
   number,
+  numberBetween,
 };
 
 export default Parsers;
