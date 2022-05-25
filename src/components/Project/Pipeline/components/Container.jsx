0b5773e2ab2c59bf0666107c41bea8cf991e5ba5
Artem Zubkov <zubkov.artem@gene.com>
2022-05-26 02:08:13 +0700

    added scrolling for pipeline page.

--- src/components/Project/Pipeline/components/Container.jsx
+++ src/components/Project/Pipeline/components/Container.jsx
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
+import { ScrollBarMixin } from '+components/ScrollBar';
 
 export default styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
   height: calc(100vh - 64px - 24px * 2);
+  overflow: hidden;
+  overflow-y: auto;
+
+  ${ScrollBarMixin};
 `;
